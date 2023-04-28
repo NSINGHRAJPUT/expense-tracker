48fe8e25e7709bfdca3019149073d523b1af7f4c
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-04-28 20:45:59 +0530

    expense tracker

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,30 +1,49 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import './Expense.css';
 
+let DUMMY_EXPENSES=[];
+
 const Expense = () =>{
+    const [exp,setExp] = useState(DUMMY_EXPENSES)
     let price = useRef(); 
     let desc = useRef();
     let category = useRef();
 
     const expenseHandler = (e)=>{
         e.preventDefault();
-        console.log(price.current.value,desc.current.value,category.current.value)
+        let catref=category.current.value
+        let obj={
+            price:price.current.value,
+            description: desc.current.value,
+            Category : catref
+        }
+        console.log(obj)
+        setExp((pre)=>[obj,...pre])
     }
 
     return <section className='expense-form'>
         <form onSubmit={expenseHandler}>
-        <label>Money Spent</label>
-        <input type='number' ref={price}></input><br/>
-        <label>Description</label>
-        <input type='text' ref={desc}></input><br/>
-        <label>Category</label>
-        <select ref={category}>
-            <option>food</option>
-            <option>petrol</option>
-            <option>Shopping</option>
-        </select><br></br>
-        <button>Submit</button>
+            <label>Money Spent</label>
+            <input type='number' ref={price}></input><br/>
+            <label>Description</label>
+            <input type='text' ref={desc}></input><br/>
+            <label>Category</label>
+            <select ref={category}>
+                <option>food</option>
+                <option>petrol</option>
+                <option>Shopping</option>
+            </select><br></br>
+            <button>Submit</button>
         </form>
+            <div>
+                {exp.map((item)=>{
+                    return <div className='expense-list'>
+                            <h3>{item.price}</h3>
+                            <h3>{item.description}</h3>
+                            <h3>{item.Category}</h3>
+                        </div>
+                })}
+            </div>
     </section>
 }
 
