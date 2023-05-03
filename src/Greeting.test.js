f753c93d4f028ce8c47559214de95922ed8874f1
NSINGHRAJPUT <nsinghrajput30@gmail.com>
2023-05-03 13:58:47 +0530

    tests

--- src/Greeting.test.js
+++ src/Greeting.test.js
@@ -4,13 +4,9 @@ import Greeting from './Greeting';
 
 describe('Greeting component', () => {
   test('renders "Hello World" as a text', () => {
-    // Arrange
+
     render(<Greeting />);
 
-    // Act
-    // ... nothing
-
-    // Assert
     const helloWorldElement = screen.getByText('Hello World!');
     expect(helloWorldElement).toBeInTheDocument();
   });
@@ -23,27 +19,25 @@ describe('Greeting component', () => {
   });
 
   test('renders "Changed!" if the button was clicked', () => {
-    // Arrange
     render(<Greeting />);
 
-    // Act
+ 
     const buttonElement = screen.getByRole('button');
     userEvent.click(buttonElement)
 
-    // Assert
+
     const outputElement = screen.getByText('Changed!');
     expect(outputElement).toBeInTheDocument();
   });
 
   test('does not render "good to see you" if the button was clicked', () => {
-     // Arrange
+
      render(<Greeting />);
 
-     // Act
+
      const buttonElement = screen.getByRole('button');
      userEvent.click(buttonElement)
  
-     // Assert
      const outputElement = screen.queryByText('good to see you', { exact: false });
      expect(outputElement).toBeNull();
   });
